de82cf4a690d174809f7cf8cd14c24fe4d57507a
Supawat Sirintranon <Boat@Supawats-MacBook-Pro.local>
2020-04-08 18:27:45 +0700

    implement service

--- src/main/java/com/sertis/test/model/CardObjRes.java
+++ src/main/java/com/sertis/test/model/CardObjRes.java
@@ -1,6 +1,18 @@
 package com.sertis.test.model;
 
 public class CardObjRes {
+    public  CardObjRes () {}
+    public CardObjRes(String cardId, String name, String status, String content, String category, String author, String responseCode, String errDesc) {
+        this.cardId = cardId;
+        this.name = name;
+        this.status = status;
+        this.content = content;
+        this.category = category;
+        this.author = author;
+        this.responseCode = responseCode;
+        this.errDesc = errDesc;
+    }
+
     private String cardId;
     private String name;
     private String status;
@@ -8,6 +20,7 @@ public class CardObjRes {
     private String category;
     private String author;
     private String responseCode;
+    private String errDesc;
 
     public String getCardId() {
         return cardId;
@@ -63,5 +76,13 @@ public class CardObjRes {
 
     public void setResponseCode(String responseCode) {
         this.responseCode = responseCode;
+    }
+
+    public String getErrDesc() {
+        return errDesc;
+    }
+
+    public void setErrDesc(String errDesc) {
+        this.errDesc = errDesc;
     }
 }
--- src/main/java/com/sertis/test/service/CardService.java
+++ src/main/java/com/sertis/test/service/CardService.java
@@ -13,16 +13,41 @@ public class CardService {
     private CardDAO cardDAO;
 
     public CardObjRes addNewCard(CardObj cardObj) {
-        return null;
+        try {
+           CardObj resCardObj = cardDAO.insertCard(cardObj);
+           return cardObjtoCardObjRes(resCardObj);
+        }catch (Exception e) {
+            return errorRes(e);
+        }
     }
 
     public CardObjRes editCard(CardObj cardObj) {
-        return null;
+        try {
+            CardObj resCardObj = cardDAO.editCard(cardObj);
+            return cardObjtoCardObjRes(resCardObj);
+        }catch (Exception e) {
+            return errorRes(e);
+        }
     }
 
     public String deleteCard(String cardId) {
-        return null;
+        try {
+            String resCardId = cardDAO.deleteCard(cardId);
+            return resCardId;
+        } catch (Exception e) {
+            return "Can't Delete";
+        }
     }
 
+    private CardObjRes cardObjtoCardObjRes(CardObj cardObj) {
+        String pass = "00";
+        return new CardObjRes(cardObj.getCardId(), cardObj.getName(), cardObj.getStatus(), cardObj.getContent(), cardObj.getCategory(), cardObj.getAuthor(),pass,null);
+    }
 
+    private CardObjRes errorRes(Exception e) {
+        CardObjRes resErr = new CardObjRes();
+        resErr.setResponseCode("10");
+        resErr.setErrDesc(e.getMessage());
+        return resErr;
+    }
 }
