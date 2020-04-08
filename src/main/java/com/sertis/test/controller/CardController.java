fe6be33401aecffa677ff94192bb97b517895870
Supawat Sirintranon <Boat@Supawats-MacBook-Pro.local>
2020-04-08 17:48:48 +0700

    added controller and service to the project

--- src/main/java/com/sertis/test/controller/CardController.java
+++ src/main/java/com/sertis/test/controller/CardController.java
@@ -1,17 +1,33 @@
 package com.sertis.test.controller;
 
 
+import com.sertis.test.model.CardObj;
 import com.sertis.test.service.CardService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class CardController {
+
     @Autowired
     private CardService cardService;
-    @PostMapping("/add/card")
-    public void addNewCard(){
 
+    @PostMapping("/add/card")
+    public void addNewCard(CardObj cardObj) {
+        cardService.addNewCard(cardObj);
     }
+
+    @PostMapping("/edit/card")
+    public void editCard(CardObj cardObj) {
+        cardService.editCard(cardObj);
+    }
+
+    @GetMapping("/delete/card/{cardId}")
+    public void deleteCard(@PathVariable String cardId) {
+        cardService.deleteCard(cardId);
+    }
+
 }
--- src/main/java/com/sertis/test/service/CardService.java
+++ src/main/java/com/sertis/test/service/CardService.java
@@ -1,4 +1,17 @@
 package com.sertis.test.service;
 
+import com.sertis.test.model.CardObj;
+
 public class CardService {
+    public void addNewCard(CardObj cardObj) {
+
+    }
+
+    public void editCard(CardObj cardObj) {
+
+    }
+
+    public void deleteCard(String cardId) {
+
+    }
 }
